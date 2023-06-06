858a2c93b7e01382024eed8df1a4699454141c0c
Shanavaas <104707139+Shanavaas@users.noreply.github.com>
2023-06-06 10:59:53 +0530

    change app name

---
+++ src/home/Home.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+export { Home };
+
+function Home() {
+  const auth = useSelector((x) => x.auth.value);
+  return (
+    <div>
+      <h1>Hi {auth?.firstName}!</h1>
+      <p>You're logged in with Dailyworks🤩</p>
+      <p>
+        <Link to="/users">Manage Users</Link>
+      </p>
+    </div>
+  );
+}
